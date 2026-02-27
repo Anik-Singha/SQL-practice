ba35c3d7108bd49a4d192070c07b216a79148d0f
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-02-27 22:45:58 +0530

    u

---
+++ 08_imp/01_past12m.sql
@@ -0,0 +1,48 @@
+/*
+Tables:
+
+orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date TIMESTAMP,
+    order_amount INT,
+    department_id INT
+);
+
+departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+
+customers (
+    customer_id INT PRIMARY KEY,
+    last_name VARCHAR(255),
+    first_name VARCHAR(255)
+);
+
+------------------------------------------------------------
+
+Question:
+
+Write a query to display:
+
+- Department names
+- Total revenue per department (SUM of order_amount)
+- Time period: Last 12 months
+- Order the result by total revenue in descending order
+
+Tables to use:
+- orders (alias: o)
+- departments (alias: d)
+
+------------------------------------------------------------
+*/
+
+select d.department_name,
+        SUM(o.order_amount) total_orders
+from departments d 
+join orders o
+on d.department_id = o.department_id
+where o.order_date >= date_sub(curdate(),interval 11 month)
+group by d.department_name
+order by total_orders desc;
